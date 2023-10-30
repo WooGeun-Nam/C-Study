b471e806a57d4ddd20eafb3930b3e9b7519d5d73
WooGeun-Nam <rolru7686@gmail.com>
2023-10-30 10:31:15 +0900

    repo init

---
+++ GameCoding/ArrayEx/GameCoding.cpp
@@ -0,0 +1,28 @@
+﻿// GameCoding.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+
+#include "Helper.h"
+#include "Map.h"
+#include "Player.h"
+
+int main()
+{
+	SetCursorOnOff(false);
+
+	// MovePlayer(3, 2);
+
+	while (true)
+	{
+		// 입력
+		HandleKeyInput();
+
+		// 로직
+		HandleMove();
+
+		// 출력
+		PrintMap2D();
+	}
+}
